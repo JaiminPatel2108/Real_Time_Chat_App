180eae631a40934a175438127d1f2db2db3e1695
Jaimin <pjaimind031@gmail.com>
2023-08-23 23:39:20 +0530

    changes in some pages

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://splendorous-toffee-e3a460.netlify.app/",
         methods: ["GET","POST"],
     }
 })
